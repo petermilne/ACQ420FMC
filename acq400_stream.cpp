46ccda5e42598975bd6de0922f80520ebf98b6b3
Peter Milne <peter.milne@d-tacq.com>
2013-07-08 13:34:41 +0100

    supports multiple sites 	modified:   acq400_stream.cpp

--- acq400_stream.cpp
+++ acq400_stream.cpp
@@ -35,6 +35,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include "popt.h"
 
 #define PROOT	"/sys/module/acq420FMC/parameters"
@@ -42,19 +43,24 @@
 static int getKnob(const char* knob, unsigned* value)
 {
 	FILE *fp = fopen(knob, "r");
-	int rc = fscanf(fp, "%u", value);
-	fclose(fp);
-	return rc;
+	if (fp){
+		int rc = fscanf(fp, "%u", value);
+		fclose(fp);
+		return rc;
+	} else {
+		return -1;
+	}
 }
 
-unsigned int nbuffers;
-unsigned int bufferlen;
+unsigned int nbuffers = 16;
+unsigned int bufferlen = 0x40000;
 int devnum = 0;
 
 int control_handle;
 
 #define BM_NULL	'n'
 #define BM_SENDFILE 's'
+#define BM_DEMUX 'd'
 int buffer_mode;
 
 int verbose;
@@ -76,6 +82,16 @@ public:
 		fd = open(fname, O_RDONLY);
 		assert(fd > 0);
 	}
+
+	Buffer(const char* _fname, int _buffer_len, bool nofile):
+		fname(_fname),
+		buffer_len(_buffer_len)
+	{
+		if (!nofile){
+			fd = open(fname, O_RDONLY);
+			assert(fd > 0);
+		}
+	}
 	static Buffer* create(const char* root, int ibuf, int _buffer_len);
 };
 
@@ -89,7 +105,7 @@ public:
 	}
 };
 class MapBuffer: public Buffer {
-private:
+protected:
 	void *pdata;
 
 public:
@@ -105,6 +121,83 @@ public:
 	}
 };
 
+#define FMT_OUT_ROOT		"/dev/shm/AI.%d.wf"
+#define FMT_OUT_ROOT_NEW	"/dev/shm/AI.%d.new"
+#define FMT_OUT_ROOT_OLD	"/dev/shm/AI.%d.old"
+
+#define NCHAN	4
+
+class DemuxBuffer: public MapBuffer {
+private:
+	int nchan;
+	int nsam;
+	short* ddata;
+	char** fnames;
+	char OUT_ROOT[128];
+	char OUT_ROOT_NEW[128];
+	char OUT_ROOT_OLD[128];
+	char CLEAN_COMMAND[128];
+	char FIN_COMMAND[128];
+
+	void make_names() {
+		sprintf(OUT_ROOT, FMT_OUT_ROOT, devnum);
+		sprintf(OUT_ROOT_NEW, FMT_OUT_ROOT_NEW, devnum);
+		sprintf(OUT_ROOT_OLD, FMT_OUT_ROOT_OLD, devnum);
+		sprintf(CLEAN_COMMAND, "rm -Rf %s", OUT_ROOT_OLD);
+		sprintf(FIN_COMMAND,   "date >%s.fin", OUT_ROOT);
+
+		fnames = new char* [nchan];
+		char buf[132];
+
+		for (int ic = 0; ic < nchan; ++ic){
+			int len = sprintf(buf, "%s/CH%02d", OUT_ROOT_NEW, ic+1);
+			fnames[ic] = new char[len+1];
+			strcpy(fnames[ic], buf);
+		}
+	}
+	void start() {
+		mkdir(OUT_ROOT_NEW, 0777);
+	}
+	void finish() {
+		system(CLEAN_COMMAND);
+		rename(OUT_ROOT, OUT_ROOT_OLD);
+		rename(OUT_ROOT_NEW, OUT_ROOT);
+		system(FIN_COMMAND);
+	}
+	void demux() {
+		short* src = static_cast<short*>(pdata);
+
+		for (int isam = 0; isam < nsam; ++isam){
+			for (int ichan = 0; ichan < nchan; ++ichan){
+				ddata[ichan*nsam + isam] = *src++;
+			}
+		}
+	}
+	void writeChan(int ic, short* src, int nsam){
+		FILE* fp = fopen(fnames[ic], "w");
+		assert(fp);
+		fwrite(src, sizeof(short), nsam, fp);
+		fclose(fp);
+	}
+public:
+	virtual int writeBuffer(int out_fd) {
+		start();
+		demux();
+		for (int ic = 0; ic < nchan; ++ic){
+			writeChan(ic, &ddata[ic*nsam], nsam);
+		}
+		finish();
+		return buffer_len;
+	}
+	DemuxBuffer(const char* _fname, int _buffer_len) :
+		MapBuffer(_fname, _buffer_len),
+		nchan(NCHAN),
+		nsam(_buffer_len/sizeof(short)/nchan)
+	{
+		ddata = new short[nsam*nchan];
+		make_names();
+	}
+};
 class TurboBuffer : public Buffer {
 private:
 	static int fake_fd;
@@ -115,7 +208,7 @@ public:
 		return sendfile(out_fd, fake_fd, 0, buffer_len);
 	}
 	TurboBuffer(const char* _fname, int _buffer_len) :
-		Buffer(_fname, _buffer_len)
+		Buffer(_fname, _buffer_len, true)
 	{
 		if (!fake_fd){
 			FILE* fp = fopen("/tmp/fakeit", "w");
@@ -146,6 +239,13 @@ Buffer* Buffer::create(const char* root, int ibuf, int _buffer_len)
 		return new NullBuffer(fname, _buffer_len);
 	case BM_SENDFILE:
 		return new TurboBuffer(fname, _buffer_len);
+	case BM_DEMUX:
+		/* HB0 only works on buffer 0 ... @@todo this could change */
+		if (ibuf == 0){
+			return new DemuxBuffer(fname, _buffer_len);
+		}else{
+			return new NullBuffer(fname, _buffer_len);
+		}
 	default:
 		return new MapBuffer(fname, _buffer_len);
 	}
@@ -153,20 +253,22 @@ Buffer* Buffer::create(const char* root, int ibuf, int _buffer_len)
 
 Buffer** buffers;
 const char* root;
+const char* stream_fmt = "%s.c";
 
 struct poptOption opt_table[] = {
-	{ "wrbuflen", 0, POPT_ARG_INT, &bufferlen, 0, 	"reduce buffer len"	},
-	{ "null-copy", 0, POPT_ARG_NONE, 0, 'n', 	"no output copy"	},
+	{ "wrbuflen", 0, POPT_ARG_INT, &bufferlen, 0, 	"reduce buffer len" },
+	{ "null-copy", 0, POPT_ARG_NONE, 0, 'n', 	"no output copy"    },
 	{ "sendfile",  0, POPT_ARG_NONE, 0, 's',
-			"use sendfile to transmit (fake)"			},
-	{ "verbose",   0, POPT_ARG_INT, &verbose, 0,  "set verbosity"		},
+			"use sendfile to transmit (fake)"		    },
+	{ "verbose",   0, POPT_ARG_INT, &verbose, 0,  "set verbosity"	    },
+	{ "hb0",       0, POPT_ARG_NONE, 0, 'h' },
 	POPT_AUTOHELP
 	POPT_TABLEEND
 };
 
 void init(int argc, const char** argv) {
-	assert(getKnob(PROOT"/nbuffers", &nbuffers) > 0);
-	assert(getKnob(PROOT"/bufferlen", &bufferlen) > 0);
+	getKnob(PROOT"/nbuffers", &nbuffers);
+	getKnob(PROOT"/bufferlen", &bufferlen);
 
 	poptContext opt_context =
 			poptGetContext(argv[0], argc, argv, opt_table, 0);
@@ -178,6 +280,10 @@ void init(int argc, const char** argv) {
 			break;
 		case 's':
 			buffer_mode = BM_SENDFILE;
+			break;
+		case 'h':
+			stream_fmt = "%s.hb0";
+			buffer_mode = BM_DEMUX;
 			break;
 		}
 	}
@@ -199,7 +305,7 @@ void init(int argc, const char** argv) {
 void stream()
 {
 	char fname[128];
-	sprintf(fname, "%s.c", root);
+	sprintf(fname, stream_fmt, root);
 	int fc = open(fname, O_RDONLY);
 	assert(fc > 0);
 
