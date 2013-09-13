f0b8aa1fadbffeb361192fb9ff8f0d9b3485dec0
Peter Milne <peter.milne@d-tacq.com>
2013-09-13 12:30:17 +0100

    add some docs 	modified:   bigmac.cpp

--- bigmac.cpp
+++ bigmac.cpp
@@ -36,7 +36,48 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+/*
+ *  benchtesting: set a long buffer and time with time(1).
+ *
+ *
+zb_012> time /tmp/bigmac -L 0x300000 -T null
+real 0m 0.06s
+user 0m 0.00s
+sys 0m 0.05s
+zb_012> time /tmp/bigmac -L 0x300000 -T cmac
+real 0m 1.10s
+user 0m 0.97s
+sys 0m 0.12s
+zb_012> time /tmp/bigmac -L 0x300000 -T nmac
+real 0m 0.54s
+user 0m 0.43s
+sys 0m 0.09s
+zb_012> time /tmp/bigmac -L 0x300000 -T nmac2
+real 0m 0.44s
+user 0m 0.32s
+sys 0m 0.10s
 
+SO: cmac : nmac : nmac2 :: 1.04 : 0.48 : 0.38
+
+[pgm@hoy3 ACQ420_FMC]$ time ./bigmac -L 0×300000 -T cmac
+
+real 0m0.078s
+user 0m0.070s
+sys 0m0.007s
+
+=> 5x faster.
+
+iop321:
+time /tmp/bigmac -L 0x300000 -T cmac
+real 0m 3.43s
+user 0m 1.43s
+sys 0m 0.99s
+
+ Compile
+  arm-xilinx-linux-gnueabi-g++ \
+  	  -mcpu=cortex-a9 -mfloat-abi=softfp -mfpu=neon -DHASNEON \
+	  -O3 -o bigmac bigmac.o -L../lib -lpopt
+ */
 
 #define NCHAN 	4
 /* mac testbench */
@@ -46,6 +87,7 @@ char *test = "null-test";
 char *outfile;
 char *infile;
 
+/* for testing: use constant gains, offsets. @@todo These need to be set from the UI */
 short gains[NCHAN] = { 5, 6, 7, 8 };
 short offsets[NCHAN] = { 100, 200, 300, 400 };
 
@@ -76,6 +118,7 @@ void cmac(short *dst, const short* src, const int nsam, const int nchan,
 #include <arm_neon.h>
 void nmac(short * dst, const short * src, const int nsam, const int nchan,
 		const short* gains, const short* offsets)
+/* replace inner loop with a 4-element vector math call */
 {
 	int tt;
 	int16x4_t GAIN = vld1_s16(gains);
@@ -90,8 +133,10 @@ void nmac(short * dst, const short * src, const int nsam, const int nchan,
 
 void nmac2(short * dst, const short * src, const int nsam, const int nchan,
 		const short* gains, const short* offsets)
+/* replace inner loop with a 8-element vector math call */
 {
 	int tt;
+	/* unfortunately we need 8-element gain, offset vectors */
 	short *g2 = new short[NCHAN*2];
 	short *o2 = new short[NCHAN*2];
 	for (int ii = 0; ii < NCHAN; ++ii){
