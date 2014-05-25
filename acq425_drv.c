679e23d5f2f25de203204606ec3cd3992dc43e0d
Peter Milne <peter.milne@d-tacq.com>
2014-05-25 13:45:05 +0100

    resolve the index vs site vs channel dilemma with macros index=site-1, channel=site+1. 	modified:   acq425_drv.c

--- acq425_drv.c
+++ acq425_drv.c
@@ -36,10 +36,11 @@ module_param_array(acq425_sites, int, &acq425_sites_count, 0644);
 static int n_acq425;
 module_param(n_acq425, int, 0444);
 
+/* adapters indexed by site .. use I2C_ADAPTER() to access */
+struct i2c_adapter *__i2c_adap[6];
 
-struct i2c_adapter *i2c_adap[7];
-
-
+#define I2C_CHAN(site) 	((site)+1)
+#define I2C_ADAPTER(site)	(__i2c_adap[(site)-1])
 
 #define PGA_TYPE	"tca6424cr"
 #define NGPIO_CHIP	24
@@ -60,24 +61,21 @@ static struct i2c_client* new_device(
 }
 static void __init acq425_init_site(int site)
 {
-	int ch = site+1;
+	I2C_ADAPTER(site) = i2c_get_adapter(I2C_CHAN(site));
 
-	i2c_adap[site] = i2c_get_adapter(ch);
-
-	if (new_device(i2c_adap[site], PGA_TYPE, 0x22, -1) == 0){
+	if (new_device(I2C_ADAPTER(site), PGA_TYPE, 0x22, -1) == 0){
 		printk("bolo8_init_site(%d) PGA1 NOT found\n", site);
 	}
 
-	if (new_device(i2c_adap[site], PGA_TYPE, 0x23, -1) == 0){
+	if (new_device(I2C_ADAPTER(site), PGA_TYPE, 0x23, -1) == 0){
 		printk("bolo8_init_site(%d) PGA2 NOT found\n", site);
 	}
 }
 
 static void __init acq425_remove_site(int site)
 {
-	int ch = site+1;
-	printk("acq425_remove_site %d channel %d\n", site, ch);
-	i2c_put_adapter(i2c_adap[site]);
+	printk("acq425_remove_site %d channel %d\n", site, I2C_CHAN(site));
+	i2c_put_adapter(I2C_ADAPTER(site));
 }
 
 static void __exit acq425_exit(void)
