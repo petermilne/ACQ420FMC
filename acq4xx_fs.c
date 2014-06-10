b2571ce4e198c3d28427a89aba565569e4d547cb
Peter Milne <peter.milne@d-tacq.com>
2014-06-10 12:19:02 +0100

    using a vfs. Confusing results with nodes randomly visible => DO NOT USE!. Reverting to cdev nodes 	modified:   acq4xx_fs.c

--- acq4xx_fs.c
+++ acq4xx_fs.c
@@ -30,6 +30,7 @@
  *
  */
 #include <linux/kernel.h>
+#include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/pagemap.h> 	/* PAGE_CACHE_SIZE */
@@ -37,14 +38,17 @@
 #include <asm/atomic.h>
 #include <asm/uaccess.h>	/* copy_to_user */
 
-/*
- * Boilerplate stuff.
- */
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Peter Milne");
+#include <linux/moduleparam.h>
+
+int file_count = 512;
+module_param(file_count, int, 0444);
 
 #define ACQ4XX_MAGIC 0xac40ac40
 
+struct BufferInfo {
+	char name[4];
+	int ix;
+};
 
 /*
  * Anytime we make a file or directory in our filesystem we need to
@@ -53,7 +57,7 @@ MODULE_AUTHOR("Peter Milne");
  * is the "mode" parameter, which says whether this is a directory
  * or file, and gives the permissions.
  */
-static struct inode *lfs_make_inode(struct super_block *sb, int mode)
+static struct inode *a4fs_make_inode(struct super_block *sb, int mode)
 {
 	struct inode *ret = new_inode(sb);
 
@@ -67,49 +71,29 @@ static struct inode *lfs_make_inode(struct super_block *sb, int mode)
 }
 
 
-/*
- * The operations on our "files".
- */
-
-/*
- * Open a file.  All we have to do here is to copy over a
- * copy of the counter pointer so it's easier to get at.
- */
-static int lfs_open(struct inode *inode, struct file *filp)
+static int a4fs_open(struct inode *inode, struct file *filp)
 {
 	filp->private_data = inode->i_private;
 	return 0;
 }
 
 #define TMPSIZE 20
-/*
- * Read a file.  Here we increment and read the counter, then pass it
- * back to the caller.  The increment only happens if the read is done
- * at the beginning of the file (offset = 0); otherwise we end up counting
- * by twos.
- */
-static ssize_t lfs_read_file(struct file *filp, char *buf,
+
+
+
+static ssize_t a4fs_read_file(struct file *filp, char *buf,
 		size_t count, loff_t *offset)
 {
-	atomic_t *counter = (atomic_t *) filp->private_data;
-	int v, len;
+	struct BufferInfo * bi = (struct BufferInfo *) filp->private_data;
+	int len;
 	char tmp[TMPSIZE];
-/*
- * Encode the value, and figure out how much of it we can pass back.
- */
-	v = atomic_read(counter);
-	if (*offset > 0)
-		v -= 1;  /* the value returned when offset was zero */
-	else
-		atomic_inc(counter);
-	len = snprintf(tmp, TMPSIZE, "%d\n", v);
+
+	len = snprintf(tmp, TMPSIZE, "%d\n", bi->ix);
 	if (*offset > len)
 		return 0;
 	if (count > len - *offset)
 		count = len - *offset;
-/*
- * Copy it back, increment the offset, and we're done.
- */
+
 	if (copy_to_user(buf, tmp + *offset, count))
 		return -EFAULT;
 	*offset += count;
@@ -119,78 +103,55 @@ static ssize_t lfs_read_file(struct file *filp, char *buf,
 /*
  * Write a file.
  */
-static ssize_t lfs_write_file(struct file *filp, const char *buf,
+static ssize_t a4fs_write_file(struct file *filp, const char *buf,
 		size_t count, loff_t *offset)
 {
-	atomic_t *counter = (atomic_t *) filp->private_data;
-	char tmp[TMPSIZE];
-/*
- * Only write from the beginning.
- */
-	if (*offset != 0)
-		return -EINVAL;
-/*
- * Read the value from the user.
- */
-	if (count >= TMPSIZE)
-		return -EINVAL;
-	memset(tmp, 0, TMPSIZE);
-	if (copy_from_user(tmp, buf, count))
-		return -EFAULT;
-/*
- * Store it in the counter and we are done.
- */
-	atomic_set(counter, simple_strtol(tmp, NULL, 10));
-	return count;
+	return -1;
 }
 
 
 /*
  * Now we can put together our file operations structure.
  */
-static struct file_operations lfs_file_ops = {
-	.open	= lfs_open,
-	.read 	= lfs_read_file,
-	.write  = lfs_write_file,
+static struct file_operations a4fs_file_ops = {
+	.open	= a4fs_open,
+	.read 	= a4fs_read_file,
+	.write  = a4fs_write_file,
 };
 
 
 /*
  * Create a file mapping a name to a counter.
  */
-static struct dentry *lfs_create_file (struct super_block *sb,
+static struct dentry *a4fs_create_file (struct super_block *sb,
 		struct dentry *dir, const char *name,
-		atomic_t *counter)
+		struct BufferInfo *info)
 {
 	struct dentry *dentry;
 	struct inode *inode;
 	struct qstr qname;
-/*
- * Make a hashed version of the name to go with the dentry.
- */
 	qname.name = name;
 	qname.len = strlen (name);
 	qname.hash = full_name_hash(name, qname.len);
-/*
- * Now we can create our dentry and the inode to go with it.
- */
+
 	dentry = d_alloc(dir, &qname);
 	if (! dentry)
 		goto out;
-	inode = lfs_make_inode(sb, S_IFREG | 0644);
+	inode = a4fs_make_inode(sb, S_IFREG | 0644);
 	if (! inode)
 		goto out_dput;
-	inode->i_fop = &lfs_file_ops;
-	inode->i_private = counter;
-/*
- * Put it all into the dentry cache and we're done.
- */
+	inode->i_fop = &a4fs_file_ops;
+	inode->i_private = info;
+
 	d_add(dentry, inode);
+
+	pr_info("created \"%s\" %p\n", name, dentry);
 	return dentry;
 /*
  * Then again, maybe it didn't work.
  */
   out_dput:
+  	pr_err("failed to create inode \"%s\"\n", name);
 	dput(dentry);
   out:
 	return 0;
@@ -202,7 +163,8 @@ static struct dentry *lfs_create_file (struct super_block *sb,
  * almost identical to the "create file" logic, except that we create
  * the inode with a different mode, and use the libfs "simple" operations.
  */
-static struct dentry *lfs_create_dir (struct super_block *sb,
+
+static struct dentry *a4fs_create_dir (struct super_block *sb,
 		struct dentry *parent, const char *name)
 {
 	struct dentry *dentry;
@@ -216,16 +178,18 @@ static struct dentry *lfs_create_dir (struct super_block *sb,
 	if (! dentry)
 		goto out;
 
-	inode = lfs_make_inode(sb, S_IFDIR | 0644);
+	inode = a4fs_make_inode(sb, S_IFDIR | 0644);
 	if (! inode)
 		goto out_dput;
 	inode->i_op = &simple_dir_inode_operations;
 	inode->i_fop = &simple_dir_operations;
 
+	pr_info("created dir \"%s\"\n", name);
 	d_add(dentry, inode);
 	return dentry;
 
   out_dput:
+  	pr_err("failed to create dir \"%s\"\n", name);
 	dput(dentry);
   out:
 	return 0;
@@ -233,26 +197,31 @@ static struct dentry *lfs_create_dir (struct super_block *sb,
 
 
 
-/*
- * OK, create the files that we export.
- */
-static atomic_t counter, subcounter;
-
-static void lfs_create_files (struct super_block *sb, struct dentry *root)
+static void a4fs_create_files (struct super_block *sb, struct dentry *root)
 {
-	struct dentry *subdir;
-/*
- * One counter in the top-level directory.
- */
-	atomic_set(&counter, 0);
-	lfs_create_file(sb, root, "counter", &counter);
-/*
- * And one in a subdirectory.
- */
-	atomic_set(&subcounter, 0);
-	subdir = lfs_create_dir(sb, root, "subdir");
-	if (subdir)
-		lfs_create_file(sb, subdir, "subcounter", &subcounter);
+	int ifile = 0;
+	struct dentry *dir = root;
+	for (ifile = 0; ifile < file_count; ++ifile){
+		struct BufferInfo *bi = kmalloc(sizeof(struct BufferInfo), GFP_KERNEL);
+		struct dentry *dentry;
+
+		if ((ifile&0x1f) == 0){
+			char* dname = kmalloc(4, GFP_KERNEL);
+			snprintf(dname, 4, "%c", ifile/0x20+'A');
+			dir = a4fs_create_dir(sb, root, dname);
+			if (dir == 0){
+				pr_err("ERROR: failed to create sub dir, use root\n");
+				dir = root;
+			}
+		}
+
+		bi->ix = ifile;
+		sprintf(bi->name, "%03d", ifile);
+
+		dentry =  a4fs_create_file (sb, dir, bi->name, bi);
+
+		pr_info("dentry %p inode %p\n", dentry, dentry? dentry->d_inode: 0x0);
+	}
 }
 
 
@@ -266,7 +235,7 @@ static void lfs_create_files (struct super_block *sb, struct dentry *root)
  * Our superblock operations, both of which are generic kernel ops
  * that we don't have to write ourselves.
  */
-static struct super_operations lfs_s_ops = {
+static struct super_operations a4fs_s_ops = {
 	.statfs		= simple_statfs,
 	.drop_inode	= generic_delete_inode,
 };
@@ -274,7 +243,7 @@ static struct super_operations lfs_s_ops = {
 /*
  * "Fill" a superblock with mundane stuff.
  */
-static int lfs_fill_super (struct super_block *sb, void *data, int silent)
+static int a4fs_fill_super (struct super_block *sb, void *data, int silent)
 {
 	struct inode *root;
 	struct dentry *root_dentry;
@@ -284,14 +253,14 @@ static int lfs_fill_super (struct super_block *sb, void *data, int silent)
 	sb->s_blocksize = PAGE_CACHE_SIZE;
 	sb->s_blocksize_bits = PAGE_CACHE_SHIFT;
 	sb->s_magic = ACQ4XX_MAGIC;
-	sb->s_op = &lfs_s_ops;
+	sb->s_op = &a4fs_s_ops;
 /*
  * We need to conjure up an inode to represent the root directory
  * of this filesystem.  Its operations all come from libfs, so we
  * don't have to mess with actually *doing* things inside this
  * directory.
  */
-	root = lfs_make_inode (sb, S_IFDIR | 0755);
+	root = a4fs_make_inode (sb, S_IFDIR | 0755);
 	if (! root)
 		goto out;
 	root->i_op = &simple_dir_inode_operations;
@@ -306,7 +275,7 @@ static int lfs_fill_super (struct super_block *sb, void *data, int silent)
 /*
  * Make up the files which will be in this filesystem, and we're done.
  */
-	lfs_create_files (sb, root_dentry);
+	a4fs_create_files (sb, root_dentry);
 	return 0;
 
   out_iput:
@@ -319,16 +288,16 @@ static int lfs_fill_super (struct super_block *sb, void *data, int silent)
 /*
  * Stuff to pass in when registering the filesystem.
  */
-static struct dentry *lfs_get_super(struct file_system_type *fst,
+static struct dentry *a4fs_get_super(struct file_system_type *fst,
 		int flags, const char *devname, void *data)
 {
-	return mount_nodev(fst, flags, data, lfs_fill_super);
+	return mount_nodev(fst, flags, data, a4fs_fill_super);
 }
 
-static struct file_system_type lfs_type = {
+static struct file_system_type a4fs_type = {
 	.owner 		= THIS_MODULE,
 	.name		= "acq4xx_fs",
-	.mount		= lfs_get_super,
+	.mount		= a4fs_get_super,
 	.kill_sb	= kill_litter_super,
 };
 
@@ -338,15 +307,18 @@ static struct file_system_type lfs_type = {
 /*
  * Get things set up.
  */
-static int __init lfs_init(void)
+static int __init a4fs_init(void)
 {
-	return register_filesystem(&lfs_type);
+	return register_filesystem(&a4fs_type);
 }
 
-static void __exit lfs_exit(void)
+static void __exit a4fs_exit(void)
 {
-	unregister_filesystem(&lfs_type);
+	unregister_filesystem(&a4fs_type);
 }
 
-module_init(lfs_init);
-module_exit(lfs_exit);
+module_init(a4fs_init);
+module_exit(a4fs_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Peter Milne");
