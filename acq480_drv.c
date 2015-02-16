2af32afb65386428441256bf228113befe777dbb
Peter Milne <peter.milne@d-tacq.com>
2015-02-16 12:06:06 +0000

    convert to platform device model 	modified:   acq480_drv.c

--- acq480_drv.c
+++ acq480_drv.c
@@ -21,14 +21,24 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
 /* ------------------------------------------------------------------------- */
 
+/*
+ * instantiates ACQ480 i2c device (50R termination)
+ * also creates a device driver hook for the spi buffer
+ * acq480_knobs app mmaps this buffer and updates it.
+ * a task on a poll loop in this driver monitors changes and sends them to SPI
+ */
 #include <linux/kernel.h>
+#include <linux/cdev.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
+#include <linux/slab.h>
 #include <linux/i2c.h>
 //#include <linux/i2c/pca953x.h>
+#include <linux/platform_device.h>
 #include <linux/platform_data/pca953x.h>
 
-#define REVID "1"
+#define REVID 		"1"
+#define MODULE_NAME	"acq480"
 
 int acq480_sites[6] = { 0,  };
 int acq480_sites_count = 0;
@@ -37,13 +47,18 @@ module_param_array(acq480_sites, int, &acq480_sites_count, 0644);
 static int n_acq480;
 module_param(n_acq480, int, 0444);
 
-/* adapters indexed by site .. use I2C_ADAPTER() to access */
-struct i2c_adapter *__i2c_adap[6];
 
 #define I2C_CHAN(site) 	((site)+1)
-#define I2C_ADAPTER(site)	(__i2c_adap[(site)-1])
-
 #define NGPIO_CHIP	8
+
+struct acq480_dev {
+	dev_t devno;
+	struct cdev cdev;
+	struct platform_device *pdev;
+	struct i2c_adapter *i2c_adapter;
+};
+
+static struct acq480_dev* acq480_devs[7];	/* 6 sites index from 1 */
 
 static struct i2c_client* new_device(
 		struct i2c_adapter *adap,
@@ -59,27 +74,74 @@ static struct i2c_client* new_device(
 	info.platform_data = &pca_data;
 	return i2c_new_device(adap, &info);
 }
-static void __init acq480_init_site(int site)
-{
-	I2C_ADAPTER(site) = i2c_get_adapter(I2C_CHAN(site));
 
-	if (new_device(I2C_ADAPTER(site), "pca9534", 0x20, -1) == 0){
+
+#define acq480_of_match 0
+
+struct acq480_dev* acq480_allocate_dev(struct platform_device *pdev)
+{
+	int site = pdev->id;
+	struct acq480_dev* adev =
+		kzalloc(sizeof(struct acq480_dev), GFP_KERNEL);
+
+	if (adev == NULL){
+		return NULL;
+	}
+	acq480_devs[site] = adev;
+
+	adev->pdev = pdev;
+	adev->i2c_adapter = i2c_get_adapter(I2C_CHAN(site));
+	if (new_device(adev->i2c_adapter, "pca9534", 0x20, -1) == 0){
 		printk("acq480_init_site(%d) PGA1 NOT found\n", site);
 	}
+
+	return adev;
+}
+
+static int acq480_probe(struct platform_device *pdev)
+{
+	struct acq480_dev* adev = acq480_allocate_dev(pdev);
+	return 0;
+}
+
+static int acq480_remove(struct platform_device *pdev)
+{
+	int site = pdev->id;
+	i2c_put_adapter(acq480_devs[site]->i2c_adapter);
+	return -1;
+}
+
+static void __init acq480_init_site(int site)
+{
+	struct platform_device* pdev =
+			kzalloc(sizeof(struct platform_device), GFP_KERNEL);
+	pdev->name = MODULE_NAME;
+	pdev->id = site;
+	platform_device_register(pdev);
 }
 
 static void __init acq480_remove_site(int site)
 {
-	printk("acq480_remove_site %d channel %d\n", site, I2C_CHAN(site));
-	i2c_put_adapter(I2C_ADAPTER(site));
+	printk("acq480_remove_site %d ERROR removal not supported\n", site);
 }
+static struct platform_driver acq480_driver = {
+        .driver = {
+                .name = MODULE_NAME,
+                .owner = THIS_MODULE,
+                .of_match_table = acq480_of_match,
+        },
+        .probe = acq480_probe,
+        .remove = acq480_remove,
+};
 
 static void __exit acq480_exit(void)
 {
 	for (; n_acq480--;){
 		acq480_remove_site(acq480_sites[n_acq480]);
 	}
+	platform_driver_unregister(&acq480_driver);
 }
+
 
 
 static int __init acq480_init(void)
@@ -88,6 +150,8 @@ static int __init acq480_init(void)
 
 
 	printk("D-TACQ ACQ425 i2c Driver %s\n", REVID);
+
+	platform_driver_register(&acq480_driver);
 
 	for (n_acq480 = 0; n_acq480 < acq480_sites_count; ++n_acq480){
 		acq480_init_site(acq480_sites[n_acq480]);
