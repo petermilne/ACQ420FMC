167349ff8fbb036474907188efd3d94fa31b27af
Peter Milne <peter.milne@d-tacq.com>
2016-01-31 16:00:20 +0000

    _another_ aggregator knob 	modified:   mgt400_sysfs.c

--- mgt400_sysfs.c
+++ mgt400_sysfs.c
@@ -335,8 +335,8 @@ static ssize_t show_agg_reg(
 		sprintf(mod_group, "sites=none");
 	}
 
-	return sprintf(buf, "0x%08x %s %s\n", regval, mod_group,
-			regval&DATA_MOVER_EN? "on": "off");
+	return sprintf(buf, "reg=0x%08x %s DATA_MOVER_EN=%s\n",
+			regval, mod_group, regval&DATA_MOVER_EN? "on": "off");
 }
 
 extern int good_sites[];
