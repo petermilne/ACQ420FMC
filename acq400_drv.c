ef4028545373a5222166598e42795544c7a5b350
Peter Milne <peter.milne@d-tacq.co.uk>
2019-08-07 11:21:57 +0100

    R3.392 : remove x400_disable_interrupt() get interrupt status. This sorry bit of old crud should really be expunged On branch k414 	modified:   acq400_drv.c

--- acq400_drv.c
+++ acq400_drv.c
@@ -24,7 +24,7 @@
 #include "dmaengine.h"
 
 
-#define REVID "3.390"
+#define REVID "3.392"
 
 /* Define debugging for use during our driver bringup */
 #undef PDEBUG
@@ -2459,7 +2459,7 @@ static irqreturn_t cos_isr(struct acq400_dev *adev)
 	acq400wr32(adev, DIO482_COS_STA, cos);
 	xtd_dev->atd.event_source = cos;
 	wake_up_interruptible(&adev->w_waitq);  /* stream_dac */
-	dev_info(DEVP(adev), "cos:0x%08x", cos);
+	dev_dbg(DEVP(adev), "cos:0x%08x", cos);
 	return IRQ_HANDLED;
 }
 
@@ -2472,12 +2472,12 @@ static irqreturn_t ao400_isr(int irq, void *dev_id)
 
 	adev->stats.interrupts++;
 	// @@todo check this.
-	x400_disable_interrupt(adev);
 	//acq420_clear_interrupt(adev, status);
 
 	if ((int_sta & DIO_INT_CSR_COS) != 0){
 		return cos_isr(adev);
 	}
+	x400_disable_interrupt(adev);
 
 	adev->stats.fifo_interrupts++;
 
