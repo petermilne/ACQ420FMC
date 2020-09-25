63e8808d37235e2ef80e8a27d0245313f933ac0e
Peter Milne <peter.milne@d-tacq.com>
2020-09-25 17:42:05 +0100

    R3.513 radcelf gps sync knobs  On branch k414 	modified:   acq400.h 	modified:   acq400_drv.c 	modified:   radcelf_sysfs.c

--- acq400_drv.c
+++ acq400_drv.c
@@ -24,7 +24,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.512"
+#define REVID 			"3.513"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
