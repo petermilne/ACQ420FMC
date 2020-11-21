44460dea465b02d22ea9d113dfe2dc17e1e9d45d
Peter Milne <peter.milne@d-tacq.com>
2020-11-21 13:52:30 +0000

    pick up IOC_HOST, monitoring is better  On branch k414 	modified:   scripts/get_eslo.py

--- scripts/get_eslo.py
+++ scripts/get_eslo.py
@@ -2,18 +2,25 @@
 import epics
 import time
 import numpy as np
-from array import array
+import os
 
-hn="acq2106_201"
 
-fmt="{}:{}:AI:CAL:ESLO"
+hn = os.getenv("IOC_HOST")
+
+fmt = "{}:{}:AI:CAL:ESLO"
 
 
 
 def record_cal(cal_pvs):
+#    print("record_cal len {}".format(len(cal_pvs)))
+    #for pv in cal_pvs:
+    #   print(pv)
     caldat = np.array([pv.get()[1:] for pv in cal_pvs])
-    flat = caldat.reshape((caldat.size))   
-    flat.tofile("/dev/shm/calblob")
+    flat = caldat.reshape((caldat.size))
+    olddat = np.fromfile("/dev/shm/calblob", dtype=np.float32)
+    if not np.array_equal(olddat, caldat):
+        print("write calblob")
+        flat.tofile("/dev/shm/calblob")
         
 def onChangeFactory(_cal_pvs, _old_state):
     cal_pvs = _cal_pvs
@@ -21,7 +28,7 @@ def onChangeFactory(_cal_pvs, _old_state):
             
     def onChange(pvname=None, value=None, char_value=None, **kw):
         nonlocal old_state, cal_pvs
-        print("{}  {} = {}".format(pvname, old_state, value))
+#        print("{}  {} = {}".format(pvname, old_state, value))
         if old_state == 0 and value == 1:
             record_cal(cal_pvs)
         old_state = value
@@ -34,8 +41,8 @@ cal_pvs = []
 sitelist_pv = epics.PV("{}:SITELIST".format(hn))
 sitelist = [ x.split('=')[0] for x in sitelist_pv.get().split(',')[1:]]
 
-cal_pvs.extend( [ epics.PV("{}:{}:AI:CAL:ESLO".format(hn, site), auto_monitor=False) for site in sitelist])
-cal_pvs.extend( [ epics.PV("{}:{}:AI:CAL:EOFF".format(hn, site), auto_monitor=False) for site in sitelist])
+cal_pvs.extend( [ epics.PV("{}:{}:AI:CAL:ESLO".format(hn, site)) for site in sitelist])
+cal_pvs.extend( [ epics.PV("{}:{}:AI:CAL:EOFF".format(hn, site)) for site in sitelist])
 record_cal(cal_pvs)
 
 state = epics.PV("{}:MODE:TRANS_ACT:STATE".format(hn))
