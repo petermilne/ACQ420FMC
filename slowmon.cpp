443b250bb36ce787f8dbfe9331cd2af735dd3e94
Peter Milne <peter.milne@d-tacq.com>
2022-03-22 08:40:36 +0000

    slowmon does nacc sum and average. nb: will need generic for int32, and protection against overflow (we're banking on a max 24bit number, max 64x oversampling -> can't overflow, but still, needs int32 template)  On branch main 	modified:   slowmon.cpp

--- slowmon.cpp
+++ slowmon.cpp
@@ -17,6 +17,9 @@
 #include <signal.h>
 
 
+/* meta data front and back. no math! */
+#define META1	sizeof(long)
+#define META2	sizeof(long)
 
 namespace G {
 	unsigned usec = 250000;
@@ -24,6 +27,13 @@ namespace G {
 	FILE* fp;
 	unsigned ndata = 64+4;
 	short *data;
+	long *sums;
+	unsigned isam = 0;
+}
+
+void clear_sums()
+{
+	memset(G::sums, 0, G::ndata*sizeof(long));
 }
 void onSample(int sig)
 {
@@ -33,13 +43,33 @@ void onSample(int sig)
 		perror("read fail");
 		exit(1);
 	}
-	write(1, G::data, len);
-}
 
+	if (G::nacc == 1){
+		write(1, G::data, len);
+	}else{
+		const int last = G::ndata-META2/sizeof(short);
+		const int first = G::ndata-META1/sizeof(short);
+
+		for (int ii = first; ii < last; ++ii){
+			G::sums[ii] += G::data[ii];
+		}
+
+		if (++G::isam == G::nacc){
+			for (int ii = first; ii < last; ++ii){
+				G::data[ii] = G::sums[ii] / G::nacc;
+			}
+			write(1, G::data, len);
+			clear_sums();
+			G::isam = 0;
+		}
+	}
+}
 
 void init(int argc, const char* argv[])
 {
 	G::data = new short[G::ndata];
+	G::sums = new long[G::ndata];
+	clear_sums();
 	G::fp = fopen("/dev/acq400.0.subr", "r");
 }
 
@@ -48,15 +78,12 @@ void ui()
 	unsigned usec;
 	unsigned fs;
 	unsigned fin;
-	unsigned nacc;
+
 	getKnob(0, "/etc/acq400/0/slowmon_fs", &fs);
 	getKnob(0, "/etc/acq400/0/slowmon_fin", &fin);
-	nacc = fs/fin;
+	G::nacc = fs/fin;
 	usec = 1000000/fs;
-	if (nacc > 1){
-		setKnob(0, "/var/log/slomonerr.log", "@@todo nacc set to 1");
-		nacc = 1;
-	}
+
 	if (usec != G::usec){
 		G::usec = usec;
 		ualarm(G::usec, G::usec);
