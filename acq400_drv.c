7a4640ad831db6e277596430f8b23c47f086cfe7
Peter Milne <peter.milne@d-tacq.com>
2022-09-09 13:29:20 +0100

    R3.714 PG wdt in decimal  On branch main2 	modified:   acq400_drv.c 	modified:   acq400_sysfs.c

--- acq400_drv.c
+++ acq400_drv.c
@@ -24,7 +24,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.713"
+#define REVID 			"3.714"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
