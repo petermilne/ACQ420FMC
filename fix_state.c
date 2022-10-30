9569e2c16a13cca80f1a0e4ba572f1c341c2a114
Peter Milne <peter.milne@d-tacq.com>
2022-10-30 16:22:26 +0000

    break into fields and reconstitute rather than magic string index  On branch main2 	modified:   fix_state.c

--- fix_state.c
+++ fix_state.c
@@ -16,14 +16,16 @@
 int main(int argc, char* argv[])
 {
 	FILE *fp = fopen(SF, "r+");
+	int args[5];
 	char data[MAXLEN];
 	if (!fp){
 		perror(SF);
 		return 1;
 	}
 	fgets(data, MAXLEN, fp);
-	if (strlen(data) >= strlen(DEFSTR)){
-		data[0] = '0';
+	if (sscanf(data, "STX %d %d %d %d %d", args+0, args+1, args+2, args+3, args+4) == 5){
+		// ensure first field is 0 aka STATE=IDLE
+		sprintf(data, "STX %d %d %d %d %d", 0, args[1], args[2], args[3], args[4]);
 	}else{
 		strcpy(data, DEFSTR);
 	}
