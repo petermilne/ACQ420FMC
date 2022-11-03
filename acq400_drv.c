264b484ce6b1507b112b4b20f56ccf18fb4883ff
Peter Milne <peter.milne@d-tacq.com>
2022-11-03 20:42:17 +0000

    R3726 add DIO422 knobs OE_CONFIG  On branch main2 	modified:   acq400.h 	modified:   acq400_debugfs.c 	modified:   acq400_drv.c 	modified:   acq400_sysfs.c

--- acq400_drv.c
+++ acq400_drv.c
@@ -24,7 +24,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.725"
+#define REVID 			"3.726"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
