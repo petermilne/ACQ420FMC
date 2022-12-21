e4ff6523670dad9dc6b8e3cca4d67aa9139b83ff
Peter Milne <peter.milne@d-tacq.com>
2022-12-21 22:26:02 +0000

    R3747 BUG fix: PWM bit fields off  On branch main2 	modified:   acq400.h

--- acq400_drv.c
+++ acq400_drv.c
@@ -23,7 +23,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.746"
+#define REVID 			"3.747"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
