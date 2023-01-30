7366fb96b2de4bddb56e5741379fbb0e8d70ecab
Peter Milne <peter.milne@d-tacq.com>
2023-01-30 18:12:45 +0000

    R3754 acq2206 HAS_HDMI_SYNC  On branch main2 	modified:   acq400.h 	modified:   acq400_drv.c

--- acq400_drv.c
+++ acq400_drv.c
@@ -23,7 +23,7 @@
 #include "dmaengine.h"
 
 
-#define REVID 			"3.753"
+#define REVID 			"3.754"
 #define MODULE_NAME             "acq420"
 
 /* Define debugging for use during our driver bringup */
