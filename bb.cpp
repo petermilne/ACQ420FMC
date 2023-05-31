3ea6d3d3b4a60e476423fad4b5b3203bf1c09fdd
Peter Milne <peter.milne@d-tacq.com>
2023-05-31 18:37:13 +0100

    /etc/acq400/0/awg_max_seg  A..Z  On branch main2 	modified:   bb.cpp

--- bb.cpp
+++ bb.cpp
@@ -53,7 +53,7 @@
 
 #include <syslog.h>
 
-#define VERID	"B1007"
+#define VERID	"B1008"
 
 #define NCHAN	4
 
@@ -104,6 +104,7 @@ namespace G {
 	char* port = 0;				// 0 no server (inetd), else make a server
 	char* host = 0;
 	char *abcde = 0;				// set start of segment
+	unsigned max_seg = 'D';
 };
 
 using namespace std;
@@ -516,7 +517,12 @@ RUN_MODE ui(int argc, const char** argv)
 	while ( (rc = poptGetNextOpt( opt_context )) >= 0 ){
 		switch(rc){
 		case 'A':
-			if (!(*G::abcde >= 'A' && *G::abcde <= 'H')){
+			getKnob(-1, "/etc/acq400/0/awg_max_seg", &G::max_seg);
+			if (!(G::max_seg >= 'A' && G::max_seg <= 'Z')){
+				 G::max_seg = 'A';
+			}
+
+			if (!(*G::abcde >= 'A' && *G::abcde <= G::max_seg)){
 				fprintf(stderr, "ERROR bad pram abcde must be A..H \%s\"\n", G::abcde);
 				exit(1);
 			}
