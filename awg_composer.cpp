8ca7b7d6786673c6e5aa0bd5e75d79f4f431196e
Peter Milne <peter.milne@d-tacq.com>
2023-08-17 13:15:03 +0100

    using MANIFEST file from build_templates. now we have all structures in place.. acq1001_590> /tmp/awg_composer 5*AA 1*BB AA,1,1536,/tmp/AWG/AA/cycloid-512-1000-11000-256-1200_1.dat AA,2,512,/tmp/AWG/AA/parabola-512-0-16000-0_2.dat AA,3,512,/tmp/AWG/AA/dc-512-0_3.dat AA,4,1024,/tmp/AWG/AA/pulse-511-1-512-10_4.dat BB,4,11,/tmp/AWG/BB/pulse-10-1-0-10_4.dat
     On branch main2
    	modified:   awg_composer.cpp

--- awg_composer.cpp
+++ awg_composer.cpp
@@ -15,21 +15,20 @@ Generic Binary file interface:
 
  */
 
-#define _POSIX_SOURCE
-#include <dirent.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#undef _POSIX_SOURCE
-
+/* fixes error stat: Value too large for defined data type */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <assert.h>
+#include <string>
 #include <vector>
 
 #include "popt.h"
+#include "split2.h"
+
+#include <iostream>
+#include <fstream>
 
 namespace G {
 	int nchan = 4;
@@ -113,6 +112,73 @@ Segment* Segment::factory(const char* segdef){
 	}
 }
 
+#define MAXNAME 128
+
+struct TemplateFile;
+typedef std::vector<TemplateFile*> VTF;
+typedef std::vector<std::string> VS;
+
+struct TemplateFile {
+	std::string seg;
+	int chan;
+	int len;
+	std::string fname;
+
+
+	TemplateFile(std::string _seg, int _chan, int _len, std::string _fname):
+		seg(_seg), chan(_chan), len(_len), fname(_fname)
+	{}
+	static TemplateFile* factory(VS fields);
+
+	std::string toString(void){
+		return seg + "," + std::to_string(chan) + "," + std::to_string(len) + "," + fname;
+	}
+};
+
+VTF template_files;
+
+#define MAXLS	65536
+
+/*
+ * [pgm@hoy5 acq400_hapi]$ cat /tmp/AWG/MANIFEST
+0 /tmp/AWG/AA/cycloid-512-1000-11000-256-1200_1.dat 1536 AA 1 cycloid-512-1000-11000-256-1200_1.dat
+1 /tmp/AWG/AA/parabola-512-0-16000-0_2.dat 512 AA 2 parabola-512-0-16000-0_2.dat
+ *
+ */
+#define MAN_IDX 	0
+#define MAN_FN  	1
+#define MAN_LENW	2
+#define MAN_SEG		3
+#define MAN_CH		4
+#define MAN_BN		5
+
+TemplateFile* TemplateFile::factory(VS fields){
+	return new TemplateFile(fields[MAN_SEG], std::stoi(fields[MAN_CH]),
+			std::stoi(fields[MAN_LENW]), fields[MAN_FN]);
+}
+void do_scan()
+{
+	std::string manifest = std::string(G::root) + "/MANIFEST";
+	std::fstream fp;
+
+	fp.open(manifest, std::ios::in);
+	assert(fp.is_open());
+
+	std::string manline;
+	while(std::getline(fp, manline)){
+		//std::cout << "Hello:" << manline << "\n";
+		VS fields;
+		split2(manline, fields, ' ');
+		if (fields[0][0] == '#'){
+			continue;
+		}else{
+
+		}
+		template_files.push_back(TemplateFile::factory(fields));
+	}
+	fp.close();
+}
+
 
 void ui(int argc, const char** argv)
 {
@@ -130,12 +196,16 @@ void ui(int argc, const char** argv)
 	while ((segdef = poptGetArg(opt_context)) != 0){
 		Segment::segments.push_back(Segment::factory(segdef));
 	}
+	do_scan();
+
+	for (auto tt: template_files){
+		std::cout << tt->toString() << std::endl;
+	}
 }
 
 int main(int argc, const char* argv[])
 {
 	ui(argc, argv);
-
 
 	for (int rep = 0; rep < G::nreps; ++rep){
 		for (Segment* segment: Segment::segments){
